394f9f5123a5b5fcd032f1ffd7f50e883cb3df80
Kseniya <tomashevich.kseniya@bk.ru>
2021-08-17 21:13:06 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+exports.min = function min(array=0) {
+    if (array.length > 0) {
+        return Math.min.apply(null, array);  
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array=0) {
+    return array.length ? Math.max.apply(null, array) : 0;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array=0) {
+    if (array.length > 0) {
+        return array.reduce((a, b) => (a + b)) / array.length;
+    }
+
+    return 0;
+};
